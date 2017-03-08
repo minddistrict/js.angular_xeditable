f40708f39d63e0620f0143ee42a7cedb25c0a43a
Richard Icke <r.icke@minddistrict.com>
2017-03-08 09:56:17 +0100

    initial commit

---
+++ js/angular_xeditable/__init__.py
@@ -0,0 +1,13 @@
+from fanstatic import Group, Library, Resource
+
+library = Library('angular-xeditable', 'resources')
+
+angular_xeditable_css = Resource(
+    library, 'css/xeditable.css',
+    minified='css/xeditable.min.css')
+
+angular_xeditable_js = Resource(
+    library, 'js/xeditable.js',
+    minified='js/xeditable.min.js')
+
+angular_xeditable = Group(angular_xeditable_js, angular_xeditable_css)
